41593065627abefc791b2fca9757c19e55f85ff9
mikio（岡村美紀） <>
2024-09-28 04:11:25 +0900

    feat: 🎸 get meisters section nice

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     // タブ切り替え機能
     $('.tab').click(function() {
         $('.tab').removeClass('active');
-        $('.content').removeClass('active');
-        $(this).addClass('active');
-        $('#' + $(this).data('tab')).addClass('active');
+        $('.education-content').removeClass('active').hide(); //これによって、activeではなくなる時に、コンテンツを消せる
+        $(this).addClass('active'); // thisっていうのが
+        $('#' + $(this).data('tab')).addClass('active').show();
     });
 
     // 色彩対比のボタン機能
@@ -17,4 +17,17 @@ $(document).ready(function() {
         myp5.changeMode('complementary');
         $('#color-explanation').text('補色対比: 赤と緑のような補色同士を並べると、互いの色が際立って見えます。');
     });
+
+    // 初期状態でColor Theoryタブをアクティブにする
+    $('.tab[data-tab="color"]').click();
+    
+    $('.modal-trigger').modaal({
+        type: 'inline',
+        // animation: 'fade',
+        // background: '#000',
+        overlay_opacity: 0.1,
+        width: 800,
+        height: 'auto'
+    });    
+
 });
