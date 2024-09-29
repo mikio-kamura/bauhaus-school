80c2447bb0cfcb8476263437eba3a7a165d83fb1
mikio（岡村美紀） <>
2024-09-29 14:00:17 +0900

    fix: 🐛 color theory design

--- script.js
+++ script.js
@@ -7,17 +7,6 @@ $(document).ready(function() {
         $('#' + $(this).data('tab')).addClass('active').show();
     });
 
-    // // 色彩対比のボタン機能
-    // $('#simultaneous-contrast').click(function() {
-    //     myp5.changeMode('simultaneous');
-    //     $('#color-explanation').text('同時対比: 同じ明度のグレーでも、周囲の色によって異なって見えます。');
-    // });
-
-    // $('#complementary-contrast').click(function() {
-    //     myp5.changeMode('complementary');
-    //     $('#color-explanation').text('補色対比: 赤と緑のような補色同士を並べると、互いの色が際立って見えます。');
-    // });
-
     // 初期状態でColor Theoryタブをアクティブにする
     $('.tab[data-tab="color"]').click();
     
